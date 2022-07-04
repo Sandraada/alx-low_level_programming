2236ae8102927e05f8846f836914297fed987367
Umunakwe <umunakweadaku@gmail.com>
2022-07-04 16:55:39 -0700

    jug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
  * main-Entry point
- * Return: Always 1 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, strl, 59);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (0);
 }

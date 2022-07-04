75a4a9ff2c34e06542d1c058a39c245fed2cb8d0
Umunakwe <umunakweadaku@gmail.com>
2022-07-04 14:26:12 -0700

    gut

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
+int d;
+for (d = 0; d < 100; d++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+if (d != 99)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
 putchar('.');
 putchar(' ');
 }

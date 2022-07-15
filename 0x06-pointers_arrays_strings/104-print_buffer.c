46b7fc2cd7e7210157e113192a5b402ba40596ba
Umunakwe <umunakweadaku@gmail.com>
2022-07-15 08:58:38 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_buffer - C function that prints the content of an
+ * inputted number of bytes from a buffer.
+ * prints 10 bytes per line.
+ * starts with the position of the first byte in hexadecimal (8 chars),
+ * starting with '0'.
+ * Each line shows the hexadecimal

762bd49128c9986659d5d8de4c73547e71458cb3
Umunakwe <umunakweadaku@gmail.com>
2022-08-02 12:47:14 -0700

    task 01

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  *
  * Return: void.
  */
-void int_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
 		return;
